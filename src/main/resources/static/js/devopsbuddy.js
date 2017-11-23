dc4d39973d84c237203bd8f51441092644057796
Omar Dawod <omardawod119@hotmail.com>
2017-11-23 14:29:47 +0100

    Added login form and login link to the navbar

--- src/main/resources/static/js/devopsbuddy.js
+++ src/main/resources/static/js/devopsbuddy.js
@@ -1,11 +1,15 @@
 $(document).ready(main);
 
 function main() {
-
+   
 	$('.btn-collapse').click(function(e) {
 		e.preventDefault(); //Prevent default button behaviour
 		var $this = $(this);
 		var $collapse = $this.closest('.collapse-group').find('.collapse');
 		$collapse.collapse('toggle');
 	});
+	
+	$(".btn").click(function(e){
+        $(this).button('loading');
+    });
 }
